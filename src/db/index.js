59fa2411662c8d05bcf0dc7f746634a7c6f01d9c
Priyanshu <priyanshupanda2019@gmail.com>
2025-03-02 18:49:47 +0530

    connect db

--- src/db/index.js
+++ src/db/index.js
@@ -6,13 +6,14 @@ const connectDB=async()=>{
      try {
           //the database is connected and respose stored in a variable
           const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}}`)
-
-          console.log(`\n MONGODB CONNECTED!!! DB HOST $connectionInstance.connection.host}`);
+          //mongoose after connectioin returns a object which is stored in connectionInstance
+          console.log(`\n MONGODB CONNECTED!!! DB HOST ${connectionInstance.connection.host}`);//look at connectionInstance in console
+          //you will get goos idea of what is happening
           
           
      } catch (error) {
           console.error("DB connection error",error);
-          process.exit(1);
+          process.exit(1);//there are different exit methods in nodejs, read about them will make you understand the code better
      }
 }
 
--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import connectDB from "./db/index.js";
 
                                    //approach 2 to connect the database(through separate file)
 // as eaarly as possible the dotenv should be configured so that the environment variables can be used
-//by eany file in the project that has its requirements.
+//by any file in the project that has its requirements.
 
 //if we use import we need to configure the dotenv file here
 //here we do the connection in index.js in db folder and call it here, so that the connection is made
 //but in approach 1 we do everything in a place and do not call it from anywhere
 
 //why use dotenv here??
-//because we need to use the environment variables in the connectDB file and we need to configure the dotenv file
+//because we need to use the environment variables in connectDB and we need to configure the dotenv file
 //so that the environment variables can be used by the connectDB file
 //if we do not use dotenv here, we need to configure the dotenv file in the connectDB file
 //and we need to configure the dotenv file in every file that uses the environment variables
@@ -36,36 +36,26 @@ connectDB()
      console.error("DB connection error",err);
      process.exit(1);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                                    //approach 1 to connect the databas
-
+                    //we donot use this as it makes the index.js here a bit clumsy
 /*import express from "express";
 const app=express();
 //use js iffe to immediately invoke the function
 //semicolon is used before it to prevent any errors
+//syntax of iffe is (function(){})()
 ;(async()=>{
      try {
           //connect mongoose to the database
           await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
           //connected the database along with its name
+          //now we use app listeners for better error handling
+
           app.on("error",(error)=>{
                console.error("ERROR",error);
                throw(error);
-          })//incase the app is not able to talk to the connnected database
+          })//incase the express app is not able to talk to the connnected database
 
+          //if no issue in connecting the database and app is able to listen to the port then->
           //listening to the promise sent by async and connecting is app was able to talk to the connected database
           app.listen(process.env.PORT||3000,()=>{
                console.log(`Server is running on port ${process.env.PORT}`);
